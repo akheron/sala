42cf9368f82e68341f90c9f139098f9a7f6d64e7
Petri Lehtinen <petri@digip.org>
2012-01-02 21:41:53 +0200

    Add a bash completion script
    Fixes #4.

---
+++ contrib/sala-completion.bash
@@ -0,0 +1,33 @@
+#!bash
+#
+# Copyright (C) 2011, 2012 Petri Lehtinen <petri@digip.org>
+#
+# sala is free software; you can redistribute it and/or modify it under
+# the terms of the MIT license. See the file LICENSE distributed with
+# the source code for details.
+#
+# The source code is available at https://github.com/akheron/sala.
+
+_sala() {
+    local IFS=$'\n' dir=${SALADIR:-.} cur
+
+    if ! type _get_comp_words_by_ref >/dev/null 2>&1; then
+        cur=${COMP_WORDS[$COMP_CWORD]}
+    else
+        _get_comp_words_by_ref cur
+    fi
+
+    # Not initialized, bail out
+    [ ! -f "$dir/.salakey" ] && return
+
+    # Skip dotfiles, e.g. .salakey
+    local names=$(cd $dir && compgen -f -- "$cur" | grep -v '^\.')
+
+    COMPREPLY=($(for name in $names; do
+        [ -d "$dir/$name" ] \
+            && echo "${name}/" \
+            || echo "$name "  # Add a space after filenames
+    done))
+}
+
+complete -o nospace -F _sala sala
